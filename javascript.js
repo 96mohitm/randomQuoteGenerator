eef88877895bbd10aba0e6c9bcebf10428e188c1
96mohitm <96mohitm@gmail.com>
2017-04-11 21:26:24 +0530

    all https changed to http

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var quote;
   var author;
   function getQuote(){
-    var url="https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+    var url="http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
     $.getJSON(url, function(data){
       tweetThis = data.quoteText + " By - " + data.quoteAuthor;
       $(".quote-text").html('"'+data.quoteText+'"');
@@ -17,13 +17,13 @@ $(document).ready(function(){
   });
   $("#tweet").on("click", function(){
     /*
-    openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));*/
+    openURL('http://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));*/
      if (tweetThis.length > 140) {
     tweetThis = "";
     $(this).addClass("disabled");
     $(this).html("Sorry! 140 chars exceeded!");
   } else {
-    $(this).attr("href", "https://twitter.com/intent/tweet?text=" + tweetThis);
+    $(this).attr("href", "http://twitter.com/intent/tweet?text=" + tweetThis);
   }
 
     });
